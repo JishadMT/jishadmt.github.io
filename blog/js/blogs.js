538d908a30f9d7c7dfcdc532fd7b494441fc69a4
Jithil Jishad MT <jithilm@zenoti.com>
2024-11-22 22:50:01 +0530

    Added new blog for C# dictionary performance

--- blog/js/blogs.js
+++ blog/js/blogs.js
@@ -1,6 +1,15 @@
 let blogs = [
     {
-        id: "1",
+        id: 3,
+        title: "Is there a better way to read value from dictionaries in C# without error?",
+        description: "Are you using TryGetValue or ContainsKey in C# dictionaries the right way? Small choices can have big impacts on performance. Discover which approach is faster, where it shines, and how it might transform your code.",
+        tags: ['c#', 'dictionary', 'benchmark'],
+        image: "csharp dictionary tryget vs containskey blog banner.png",
+        date: "22 November, 2024",
+        link: "csharp-dictionary-containskey-vs-trygetvalue-benchmark.html",
+    },
+    {
+        id: 2,
         title: "Git is not picking up changes when renamed a file with upper case to lower case change?",
         description: "Ever renamed a file in Visual Studio from uppercase to lowercase, only to find Git doesn’t recognize the change? This case-insensitive quirk can disrupt your version control. Here’s a quick, foolproof way to fix it, ensuring smooth tracking across all systems.",
         tags: ['git', 'visual-studio', 'github'],
@@ -9,14 +18,14 @@ let blogs = [
         link: "renamed-file-not-picked-by-git-changes.html",
     },
     {
-        id: "2",
+        id: 1,
         title: "How to delete all files in a folder ending with specific name?",
         description: "Managing files from the Command Prompt (CMD) can be a real time-saver, especially when you need to delete multiple files with a specific naming pattern. Let's find out how.",
         tags: ['cmd', 'windows', 'file'],
         image: "del cmd.png",
         date: "09 November, 2024",
         link: "delete-files-from-command-promt-ending-with-specific-name.html",
-    }
+    }    
 ]
 
 
